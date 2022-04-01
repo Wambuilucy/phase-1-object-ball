86fa5ddadc56c8c96b9e69e9d60bcd21205334ff
Wambuilucy <wambuilucie99@gmail.com>
2022-04-02 00:50:09 +0300

    lab complete

---
+++ src/00-objectball.js
@@ -0,0 +1,16 @@
+let gameObject = () => {
+	const obj = {
+		BMHS: {
+			teamName: "Glee",
+			colors: ["Pink", "White"],
+			players: {
+				"Don Doe": {
+					number: 0,
+					shoe: 16,
+					points: 22,
+					rebounds: 12,
+					assists: 12,
+					steals: 3,
+					blocks: 1,
+					slamDunks: 1
+				},
